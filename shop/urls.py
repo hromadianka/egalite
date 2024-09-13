f5dd9d4edf150fe07f0a2f6e49fe12c763ae956f
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-09-13 16:23:32 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import shop_view, product_view
+from .views import shop_view, product_view, add_to_cart, remove_from cart
 
 urlpatterns = [
     path('', shop_view, name='shop'),
     path('<int:product_id>/', product_view, name='product'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
 ]
 
