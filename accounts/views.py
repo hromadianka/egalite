85718cdb853d63ae7614ba30ca85777be17691fd
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-09-13 12:56:47 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create(
+            username=username,
+            email=email,
+            password=make_password(password)
+        )
+        login(request, user)
+        return redirect('about')
+    return render(request, 'register.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
