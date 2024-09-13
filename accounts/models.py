3d8faf6f31fa3c8f5f6c160cb3306cf129e7c365
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-09-13 13:05:49 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from shop.models import Product
+from feed.models import Publication
 
 # Create your models here.
 
@@ -7,16 +9,8 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.user.username
-
-class Editor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    can_edit_about = models.BooleanField(default=False)
-    can_manage_products = models.BooleanField(default=False)
-    can_manage_orders = models.BooleanField(default=False)
-    can_manage_publications = models.BooleanField(default=False)
+    saved_posts = models.ManyToManyField(Publication, blank=True)
+    saved_products = models.ManyToManyField(Product, blank=True)
 
     def __str__(self):
         return self.user.username
