0754f64e937f1b687012d8d2f12db670945142fa
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-09-13 13:37:14 +0300

    Update admin.py

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AboutUs, ResourseLink, Store
 
 # Register your models here.
+
+admin.site.register(AboutUs)
+admin.site.register(ResourceLink)
+admin.site.register(Store)
