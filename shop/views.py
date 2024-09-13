c775fe485f0a30ac4f55da55b6ffd689ab87f271
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-09-13 16:28:47 +0300

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Cart, CartItem
 
 # Create your views here.
 
@@ -10,3 +10,20 @@ def shop_view(request):
 def product_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'shop/product.html', {'product': product})
+
+def add_to_cart(request, product_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    else:
+        cart_item.quantity = 1
+        cart_item.save()
+
+    return redirect('product_view')
