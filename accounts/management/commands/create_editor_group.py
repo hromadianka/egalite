cd3974e0a96264bbb6fd4ec81cf5481e1b6e7b9b
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-09-13 13:24:01 +0300

    Update create_editor_group.py

--- accounts/management/commands/create_editor_group.py
+++ accounts/management/commands/create_editor_group.py
@@ -1,1 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
 
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        editor_group, created = Group.objects.get_or_create(name='Editors')
+        if created:
+            permissions = Permission.objects.filter(
+            )
+            editor_group.permissions.set(permissions)
+            self.stdout.write(self.style.SUCCESS('Successfully created "Editors" group with specified permissions.'))
+        else:
+            self.stdout.write(self.style.WARNING('"Editors" group already exists.'))
